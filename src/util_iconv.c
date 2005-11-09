764b1ac466650cedf559c0eaa58bc0672a311ec9
Benjamin Zores <ben@geexbox.org>
2005-11-09 07:50:31 +0000

    fix iconv_util headers

--- src/util_iconv.c
+++ src/util_iconv.c
@@ -1,8 +1,7 @@
 /*
- * ushare.c : GeeXboX uShare UPnP Media Server.
+ * util_iconv.c : GeeXboX uShare iconv string encoding utlities.
  * Originally developped for the GeeXboX project.
- * Parts of the code are originated from GMediaServer from Oskar Liljeblad.
- * Copyright (C) 2005 Benjamin Zores <ben@geexbox.org>
+ * Copyright (C) 2005 Alexis Saettler <asbin@asbin.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
