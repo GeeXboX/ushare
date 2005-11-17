570bdf31c60693596f2b9e7e1ef372d4a40ed442
Benjamin Zores <ben@geexbox.org>
2005-11-17 20:07:15 +0000

    compiles nicely with GCC -ansi flag

--- src/http.c
+++ src/http.c
@@ -19,6 +19,7 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
+#define _GNU_SOURCE
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
--- src/ushare.c
+++ src/ushare.c
@@ -19,6 +19,7 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <signal.h>
 #include <string.h>
