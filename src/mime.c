174526c50c9aedd678ecc4033ef01e40d020e6fa
Benjamin Zores <ben@geexbox.org>
2005-11-20 14:52:59 +0000

    use FreeDesktop official MIME type naming convention

--- src/mime.c
+++ src/mime.c
@@ -1,7 +1,7 @@
 /*
  * mime.c : GeeXboX uShare media file MIME-type association.
  * Originally developped for the GeeXboX project.
- * Parts of the code are originated from GMediaServer from Oskar Liljeblad.
+ * Ref : http://freedesktop.org/wiki/Standards_2fshared_2dmime_2dinfo_2dspec
  * Copyright (C) 2005 Benjamin Zores <ben@geexbox.org>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -26,17 +26,17 @@
 struct mime_type_t MIME_Type_List[] = {
   /* Video files */
   { "asf", "object.item.videoItem.movie", "http-get:*:video/x-ms-asf:*"},
-  { "avi", "object.item.videoItem.movie", "http-get:*:video/x-msvideo:*"},
+  { "avi", "object.item.videoItem.movie", "http-get:*:video/avi:*"},
   { "divx", "object.item.videoItem.movie", "http-get:*:video/x-msvideo:*"},
   { "wmv", "object.item.videoItem.movie", "http-get:*:video/x-ms-wmv:*"},
   { "mpeg", "object.item.videoItem.movie", "http-get:*:video/mpeg:*"},
   { "mpg", "object.item.videoItem.movie", "http-get:*:video/mpeg:*"},
   { "mpe", "object.item.videoItem.movie", "http-get:*:video/mpeg:*"},
-  { "vob", "object.item.videoItem.movie", "http-get:*:video/mpeg:*"},
+  { "vob", "object.item.videoItem.movie", "http-get:*:video/mp2p:*"},
   { "m1v", "object.item.videoItem.movie", "http-get:*:video/mpeg:*"},
-  { "m2v", "object.item.videoItem.movie", "http-get:*:video/mpeg:*"},
-  { "m4v", "object.item.videoItem.movie", "http-get:*:video/mpeg:*"},
-  { "ts", "object.item.videoItem.movie", "http-get:*:video/mpeg:*"},
+  { "m2v", "object.item.videoItem.movie", "http-get:*:video/mpeg2:*"},
+  { "m4v", "object.item.videoItem.movie", "http-get:*:video/mp4:*"},
+  { "ts", "object.item.videoItem.movie", "http-get:*:video/mpeg2:*"},
   { "ogm", "object.item.videoItem.movie", "http-get:*:video/mpeg:*"},
   { "mkv", "object.item.videoItem.movie", "http-get:*:video/mpeg:*"},
   { "rmvb", "object.item.videoItem.movie", "http-get:*:video/mpeg:*"},
@@ -44,13 +44,12 @@ struct mime_type_t MIME_Type_List[] = {
   { "qt", "object.item.videoItem.movie", "http-get:*:video/quicktime:*"},
 
   /* Audio files */
-  { "mp2", "object.item.audioItem.musicTrack", "http-get:*:audio/mpeg:*"},
+  { "mp2", "object.item.audioItem.musicTrack", "http-get:*:audio/mp2:*"},
   { "mp3", "object.item.audioItem.musicTrack", "http-get:*:audio/mpeg:*"},
-  { "mp4", "object.item.audioItem.musicTrack", "http-get:*:audio/mpeg:*"},
+  { "mp4", "object.item.audioItem.musicTrack", "http-get:*:audio/mp4:*"},
   { "m4a", "object.item.audioItem.musicTrack", "http-get:*:audio/mpeg:*"},
-  { "ogg", "object.item.audioItem.musicTrack",
-    "http-get:*:audio/application/ogg:*"},
-  { "wav", "object.item.audioItem.musicTrack", "http-get:*:audio/x-wav:*"},
+  { "ogg", "object.item.audioItem.musicTrack", "http-get:*:audio/x-ogg:*"},
+  { "wav", "object.item.audioItem.musicTrack", "http-get:*:audio/wav:*"},
   { "wma", "object.item.audioItem.musicTrack",
     "http-get:*:audio/audio/x-ms-wma:*"},
   { "mka", "object.item.audioItem.musicTrack", "http-get:*:audio/mpeg:*"},
@@ -62,7 +61,7 @@ struct mime_type_t MIME_Type_List[] = {
     "http-get:*:audio/x-pn-realaudio:*"},
 
   /* Images files */
-  { "bmp", "object.item.imageItem.photo", "http-get:*:image/x-ms-bmp:*"},
+  { "bmp", "object.item.imageItem.photo", "http-get:*:image/bmp:*"},
   { "gif", "object.item.imageItem.photo", "http-get:*:image/gif:*"},
   { "jpeg", "object.item.imageItem.photo", "http-get:*:image/jpeg:*"},
   { "jpg", "object.item.imageItem.photo", "http-get:*:image/jpeg:*"},
